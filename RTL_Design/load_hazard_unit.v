85e9a339e96af56dc2ebeacb6e388017c663657d
Rakshith Kothuru <163036593+RakshithKothuru@users.noreply.github.com>
2025-08-17 14:32:34 +0530

    Update load_hazard_unit.v

--- RTL_Design/load_hazard_unit.v
+++ RTL_Design/load_hazard_unit.v
@@ -1,5 +1,5 @@
 module load_hazard_unit(
-    input ResultSrcE,      // 1 if EX-stage instruction is a load (Result from memory)
+    input [1:0] ResultSrcE,      // 01 if EX-stage instruction is a load (Result from memory)
     input [4:0] RD_E,      // destination register in EX stage
     input [31:0] InstrD,   // IF/ID instruction word (to read rs1/rs2)
     output pc_write,
@@ -8,7 +8,7 @@ module load_hazard_unit(
 );
 
     wire load_in_ex;
-    assign load_in_ex = ResultSrcE; // in this design ResultSrc==1 for loads
+    assign load_in_ex = (ResultSrcE == 2'b01); // in this design ResultSrc==01 for loads
 
     wire [4:0] rs1_d = InstrD[19:15];
     wire [4:0] rs2_d = InstrD[24:20];
@@ -17,7 +17,7 @@ module load_hazard_unit(
     assign hazard = load_in_ex & (RD_E != 5'h00) & ((RD_E == rs1_d) | (RD_E == rs2_d));
 
     assign pc_write = ~hazard;
-    assign stallF_load_hazard = ~hazard;
+    assign stallF_load_hazard = hazard;
     assign flushD_load_hazard = hazard; // turns ID/EX into NOP for one cycle
 
 endmodule
