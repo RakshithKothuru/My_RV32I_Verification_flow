e678b3fc7124c87218ae3fc6c54d86ca35bc34af
Rakshith Kothuru <163036593+RakshithKothuru@users.noreply.github.com>
2025-08-17 14:27:42 +0530

    Update Sign_Extend.v

--- RTL_Design/Sign_Extend.v
+++ RTL_Design/Sign_Extend.v
@@ -1,15 +1,16 @@
 module Sign_Extend (In,ImmSrc,Imm_Ext);
     input [31:0] In;
-    input [1:0] ImmSrc;
+    input [2:0] ImmSrc;
     output reg [31:0] Imm_Ext;
 
    always @(*) begin
     case (ImmSrc)
-        2'b00:   Imm_Ext = {{20{In[31]}}, In[31:20]};                         // I-type
-        2'b01:   Imm_Ext = {{20{In[31]}}, In[31:25], In[11:7]};               // S-type
-        2'b10:   Imm_Ext = {{20{In[31]}}, In[7], In[30:25], In[11:8], 1'b0};  // B-type
-        2'b11:   Imm_Ext = 32'h00000000;                                      // All zeros
-        default: Imm_Ext = 32'h00000000;                                      // Safety
+        3'b000:   Imm_Ext = {{20{In[31]}}, In[31:20]};                           // L-type
+        3'b001:   Imm_Ext = {{20{In[31]}}, In[31:25], In[11:7]};                 // S-type
+        3'b010:   Imm_Ext = {{20{In[31]}}, In[7], In[30:25], In[11:8], 1'b0};    // B-type
+        3'b011:   Imm_Ext = {{12{In[31]}}, In[19:12], In[20], In[30:21], 1'b0};  // J-type  
+        3'b100:   Imm_Ext = {{20{In[31]}}, In[31:20]};                           // I-type
+        default:  Imm_Ext = 32'h00000000;                                        // Safety
     endcase
 end
 
